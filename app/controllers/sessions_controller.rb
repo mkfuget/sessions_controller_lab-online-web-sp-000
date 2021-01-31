f7c3e57f00efb674b7ee6fa6add41078ca570738
Michael Fuget <michaelfuget@gmail.com>
2021-01-31 20:06:00 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,11 @@
 class SessionsController < ApplicationController
+  def new
+      # nothing to do here!
+  end
+
+  def create
+      session[:username] = params[:username]
+      redirect_to '/'
+  end
 
 end
